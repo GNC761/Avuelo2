3157fb9938a89fe4ecf899190134bb89ded1b429
Guilherme Neves Canedo <guinc159@gmail.com>
2021-02-13 16:44:01 -0300

    STARTED INICIAL SCREEN

---
+++ avuelo-project-main/src/helpers/fake-backend.js
@@ -0,0 +1,112 @@
+export function configureFakeBackend() {
+    let users = [{ id: 1, username: 'test', password: 'test', firstName: 'Test', lastName: 'User', role: 'Admin' }];
+    let realFetch = window.fetch;
+    window.fetch = function(url, opts) {
+        return new Promise((resolve, reject) => {
+            // wrap in timeout to simulate server api call
+            setTimeout(() => {
+                // authenticate
+                if (url.endsWith('/users/authenticate') && opts.method === 'POST') {
+                    // get parameters from post request
+                    let params = JSON.parse(opts.body);
+
+                    // find if any user matches login credentials
+                    let filteredUsers = users.filter(user => {
+                        return user.username === params.username && user.password === params.password;
+                    });
+
+                    if (filteredUsers.length) {
+                        // if login details are valid return user details and fake jwt token
+                        let user = filteredUsers[0];
+                        let responseJson = {
+                            id: user.id,
+                            username: user.username,
+                            firstName: user.firstName,
+                            lastName: user.lastName,
+                            role: 'Admin',
+                            token:
+                                'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJjb2RlcnRoZW1lcyIsImlhdCI6MTU4NzM1NjY0OSwiZXhwIjoxOTAyODg5NDQ5LCJhdWQiOiJjb2RlcnRoZW1lcy5jb20iLCJzdWIiOiJzdXBwb3J0QGNvZGVydGhlbWVzLmNvbSIsImxhc3ROYW1lIjoiVGVzdCIsIkVtYWlsIjoic3VwcG9ydEBjb2RlcnRoZW1lcy5jb20iLCJSb2xlIjoiQWRtaW4iLCJmaXJzdE5hbWUiOiJIeXBlciJ9.P27f7JNBF-vOaJFpkn-upfEh3zSprYfyhTOYhijykdI',
+                        };
+                        resolve({ ok: true, json: () => responseJson });
+                    } else {
+                        // else return error
+                        reject('Usuario ou senha incorretos.');
+                    }
+                    return;
+                }
+
+                // register
+                if (url.endsWith('/users/register') && opts.method === 'POST') {
+                    // get parameters from post request
+                    let params = JSON.parse(opts.body);
+
+                    // add new users
+                    let { firstName, lastName } = params.fullname.split(' ');
+                    let newUser = {
+                        id: users.length + 1,
+                        username: firstName,
+                        password: params.password,
+                        firstName: firstName,
+                        lastName: lastName,
+                        role: 'Admin',
+                    };
+                    users.push({ newUser });
+
+                    let responseJson = {
+                        id: newUser.id,
+                        username: newUser.username,
+                        firstName: newUser.firstName,
+                        lastName: newUser.lastName,
+                        role: newUser.role,
+                        token:
+                            'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJjb2RlcnRoZW1lcyIsImlhdCI6MTU4NzM1NjY0OSwiZXhwIjoxOTAyODg5NDQ5LCJhdWQiOiJjb2RlcnRoZW1lcy5jb20iLCJzdWIiOiJzdXBwb3J0QGNvZGVydGhlbWVzLmNvbSIsImxhc3ROYW1lIjoiVGVzdCIsIkVtYWlsIjoic3VwcG9ydEBjb2RlcnRoZW1lcy5jb20iLCJSb2xlIjoiQWRtaW4iLCJmaXJzdE5hbWUiOiJIeXBlciJ9.P27f7JNBF-vOaJFpkn-upfEh3zSprYfyhTOYhijykdI',
+                    };
+                    resolve({ ok: true, json: () => responseJson });
+                    return;
+                }
+
+                // forget password
+                if (url.endsWith('/users/password-reset') && opts.method === 'POST') {
+                    // get parameters from post request
+                    let params = JSON.parse(opts.body);
+
+                    // find if any user matches login credentials
+                    let filteredUsers = users.filter(user => {
+                        return user.username === params.username;
+                    });
+
+                    if (filteredUsers.length) {
+                        let responseJson = {
+                            message: "Uma mensagem foi enviada para o email informado. Acesse o link no email para atualizar sua senha.",
+                        };
+                        resolve({ ok: true, json: () => responseJson });
+                    } else {
+                        // else return error
+                        reject('Não encontramos nenhum usuario com o email informado. Tente novamente');
+                    }
+                    return;
+                }
+
+                // get users
+                if (url.endsWith('/users') && opts.method === 'GET') {
+                    // check for fake auth token in header and return users if valid, this security is implemented server side in a real application
+                    if (
+                        opts.headers &&
+                        opts.headers.Authorization ===
+                            'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJjb2RlcnRoZW1lcyIsImlhdCI6MTU4NzM1NjY0OSwiZXhwIjoxOTAyODg5NDQ5LCJhdWQiOiJjb2RlcnRoZW1lcy5jb20iLCJzdWIiOiJzdXBwb3J0QGNvZGVydGhlbWVzLmNvbSIsImxhc3ROYW1lIjoiVGVzdCIsIkVtYWlsIjoic3VwcG9ydEBjb2RlcnRoZW1lcy5jb20iLCJSb2xlIjoiQWRtaW4iLCJmaXJzdE5hbWUiOiJIeXBlciJ9.P27f7JNBF-vOaJFpkn-upfEh3zSprYfyhTOYhijykdI'
+                    ) {
+                        resolve({ ok: true, json: () => users });
+                    } else {
+                        // return 401 not authorised if token is null or invalid
+                        reject('Unauthorised');
+                    }
+
+                    return;
+                }
+
+                // pass through any requests not handled above
+                realFetch(url, opts).then(response => resolve(response));
+            }, 500);
+        });
+    };
+}
---
+++ avuelo-project-main/src/pages/auth/Register.js
@@ -0,0 +1,158 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Redirect, Link } from 'react-router-dom';
+
+import { Container, Row, Col, Card, CardBody, Label, FormGroup, Button, Alert } from 'reactstrap';
+import { AvForm, AvField, AvGroup, AvInput, AvFeedback } from 'availity-reactstrap-validation';
+
+import { registerUser } from '../../redux/actions';
+import { isUserAuthenticated } from '../../helpers/authUtils';
+import LoaderWidget from '../../components/Loader';
+import logo from '../../assets/images/logo.png';
+
+class Register extends Component {
+    _isMounted = false;
+
+    constructor(props) {
+        super(props);
+
+        this.handleValidSubmit = this.handleValidSubmit.bind(this);
+    }
+
+    componentDidMount() {
+        this._isMounted = true;
+    }
+
+    componentWillUnmount() {
+        this._isMounted = false;
+    }
+
+    /**
+     * Handles the submit
+     */
+    handleValidSubmit = (event, values) => {
+        this.props.registerUser(values.fullname, values.email, values.password);
+    };
+
+    /**
+     * Redirect to root
+     */
+    renderRedirectToRoot = () => {
+        const isAuthTokenValid = isUserAuthenticated();
+        if (isAuthTokenValid) {
+            return <Redirect to="/" />;
+        }
+    };
+
+    /**
+     * Redirect to confirm
+     */
+    renderRedirectToConfirm = () => {
+        return <Redirect to="confirm" />;
+    };
+
+    render() {
+        const isAuthTokenValid = isUserAuthenticated();
+        return (
+            <React.Fragment>
+                {this.renderRedirectToRoot()}
+
+                {this.props.user && this.renderRedirectToConfirm()}
+
+                {(this._isMounted || !isAuthTokenValid) && (
+                    <div className="account-pages mt-5 mb-5">
+                        <Container>
+                            <Row className="justify-content-center">
+                                <Col lg={5}>
+                                    <Card>
+                                        <div className="card-header pt-4 pb-4 text-center bg-primary">
+                                            <Link to="/">
+                                                <span>
+                                                    <img src={logo} alt="" height="18" />
+                                                </span>
+                                            </Link>
+                                        </div>
+
+                                        <CardBody className="p-4 position-relative">
+                                            {/* preloader */}
+                                            {this.props.loading && <LoaderWidget />}
+
+                                            <div className="text-center w-75 m-auto">
+                                                <h4 className="text-dark-50 text-center mt-0 font-weight-bold">
+                                                    Registrar-se
+                                                </h4>
+                                                <p className="text-muted mb-4">
+                                                    Não tem uma conta? Crie uma. Leva menos de um minuto
+                                                </p>
+                                            </div>
+
+                                            {this.props.error && (
+                                                <Alert color="danger" isOpen={this.props.error ? true : false}>
+                                                    <div>{this.props.error}</div>
+                                                </Alert>
+                                            )}
+
+                                            <AvForm onValidSubmit={this.handleValidSubmit}>
+                                                <AvField
+                                                    name="fullname"
+                                                    label="Nome completo"
+                                                    placeholder="Digite seu nome"
+                                                    required
+                                                />
+
+                                                <AvField
+                                                    type="email"
+                                                    name="email"
+                                                    label="Email"
+                                                    placeholder="Digite seu endereço de email"
+                                                    required
+                                                />
+
+                                                <AvGroup>
+                                                    <Label for="password">Senha</Label>
+                                                    <AvInput
+                                                        type="password"
+                                                        name="password"
+                                                        id="password"
+                                                        placeholder="Digite sua senha"
+                                                        required
+                                                    />
+                                                    <AvFeedback>Valor invalido</AvFeedback>
+                                                </AvGroup>
+
+                                                <FormGroup>
+                                                    <Button color="success">Enviar</Button>
+                                                </FormGroup>
+                                            </AvForm>
+                                        </CardBody>
+                                    </Card>
+                                </Col>
+                            </Row>
+
+                            <Row className="mt-1">
+                                <Col className="col-12 text-center">
+                                    <p className="text-muted">
+                                        Já tem uma conta?{' '}
+                                        <Link to="/account/login" className="text-muted ml-1">
+                                            <b>Entrar</b>
+                                        </Link>
+                                    </p>
+                                </Col>
+                            </Row>
+                        </Container>
+                    </div>
+                )}
+            </React.Fragment>
+        );
+    }
+}
+
+const mapStateToProps = state => {
+    const { user, loading, error } = state.Auth;
+    return { user, loading, error };
+};
+
+export default connect(
+    mapStateToProps,
+    { registerUser }
+)(Register);
